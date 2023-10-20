99b58e07502d5b0a45c2cc46e1081fd9be04a94c
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-10-20 18:27:18 -0300

    custom image upload

--- src/pages/Products/EditProduct/index.tsx
+++ src/pages/Products/EditProduct/index.tsx
@@ -1,17 +1,21 @@
-import { FormControl, FormLabel } from "@chakra-ui/form-control";
-import { Input } from "@chakra-ui/input";
-import { useParams } from "react-router-dom";
-import style from "./style.module.css";
-import { ProductsType } from "../../../types/Products";
-import { useEffect, useState } from "react";
-import { GetProductById } from "./service";
-import { Image } from "@chakra-ui/image";
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Card, CardBody, CardHeader } from "@chakra-ui/card";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Image } from "@chakra-ui/image";
+import { Input } from "@chakra-ui/input";
 import { Text } from "@chakra-ui/layout";
-import { priceFormat } from "../../../services/priceFormat";
+import { Box, Select } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { NumericFormat } from "react-number-format";
+import { useParams } from "react-router-dom";
+import { ProductType } from "../context/ProductContext";
+import { GetCategories, GetProductById } from "./service";
+import emptyImage from "../../../assets/empty-image.svg";
+import style from "./style.module.css";
 
 export default function EditProduct() {
-  const [product, setProduct] = useState<ProductsType>();
+  const [product, setProduct] = useState<ProductType>();
+  const [categories, setCategories] = useState<string[]>();
 
   const { id } = useParams();
 
@@ -19,91 +23,209 @@ export default function EditProduct() {
     GetProductById(Number(id)).then((res) => {
       setProduct(res);
     });
+
+    GetCategories().then((res) => {
+      setCategories(res);
+    });
   }, []);
+
+  function handleChange(
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ) {
+    e.preventDefault();
+    setProduct({ ...product, [e.target.name]: e.target.value } as ProductType);
+  }
+
+  function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files && e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProduct({
+          ...product,
+          [e.target.name]: reader.result as string,
+        } as ProductType);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 
   return (
     <>
-      <Text className={style.title}>Editar Produto</Text>
-      <FormControl padding="0 100px">
-        <div className={style.container}>
-          <div>
-            <FormLabel>Nome</FormLabel>
-            <Input w={300} type="text" value={product?.title} />
-          </div>
-
-          <div>
-            <FormLabel>Marca</FormLabel>
-            <Input w={300} type="text" value={product?.brand} />
-          </div>
-
-          <div>
-            <FormLabel>Descrição</FormLabel>
-            <Input w={300} type="text" value={product?.description} />
-          </div>
-
-          <div>
-            <FormLabel>Preço</FormLabel>
-            <Input
-              w={300}
-              type="text"
-              value={product?.price && priceFormat(product.price)}
-            />
-          </div>
-
-          <div>
-            <FormLabel>Porcentagem de desconto</FormLabel>
-            <Input w={300} type="text" value={product?.discountPercentage} />
-          </div>
-
-          <div>
-            <FormLabel>Categoria</FormLabel>
-            <Input w={300} type="text" value={product?.category} />
-          </div>
-
-          <div>
-            <FormLabel>Avaliação</FormLabel>
-            <Input w={300} type="text" value={product?.rating} />
-          </div>
-
-          <div>
-            <FormLabel>Estoque</FormLabel>
-            <Input w={300} type="text" value={product?.stock} />
-          </div>
-        </div>
-
-        <div className={style.container} style={{ marginTop: "30px" }}>
-          <Card maxW="auto">
-            <CardHeader>
-              <Text as="b">Imagem principal</Text>
-            </CardHeader>
-            <CardBody display="flex" justifyContent="center">
-              <Image
-                src={product?.thumbnail}
-                alt={product?.title}
-                width={300}
-                height="auto"
+      <Box
+        borderWidth="1px"
+        rounded="lg"
+        shadow="1px 1px 3px rgba(0,0,0,0.3)"
+        maxWidth="90vw"
+        minWidth={"500px"}
+        p={6}
+        m="10px auto"
+      >
+        <Text className={style.title}>Editar Produto</Text>
+        <FormControl padding="0 100px">
+          <div className={style.container}>
+            <div>
+              <FormLabel>Nome</FormLabel>
+              <Input
+                onChange={handleChange}
+                w={300}
+                type="text"
+                name="title"
+                value={product?.title}
               />
-            </CardBody>
-          </Card>
+            </div>
 
-          <Card maxW="auto">
-            <CardHeader>
-              <Text as="b">Imagens</Text>
-            </CardHeader>
-            <CardBody display="flex" justifyContent="center">
-              {product?.images.map((image) => (
+            <div>
+              <FormLabel>Marca</FormLabel>
+              <Input
+                onChange={handleChange}
+                w={300}
+                type="text"
+                name="brand"
+                value={product?.brand}
+              />
+            </div>
+
+            <div>
+              <FormLabel>Descrição</FormLabel>
+              <Input
+                onChange={handleChange}
+                w={300}
+                type="text"
+                name="description"
+                value={product?.description}
+              />
+            </div>
+
+            <div>
+              <FormLabel>Preço</FormLabel>
+              <NumericFormat
+                onChange={handleChange}
+                value={product?.price}
+                name="price"
+                decimalScale={2}
+                fixedDecimalScale
+                decimalSeparator=","
+                thousandSeparator="."
+                customInput={Input}
+                w={300}
+                prefix="R$ "
+              />
+            </div>
+
+            <div>
+              <FormLabel>Porcentagem de desconto</FormLabel>
+              <NumericFormat
+                onChange={handleChange}
+                type="text"
+                name="discountPercentage"
+                value={product?.discountPercentage}
+                customInput={Input}
+                w={300}
+                decimalScale={2}
+                fixedDecimalScale
+                decimalSeparator=","
+                thousandSeparator="."
+                suffix=" %"
+              />
+            </div>
+
+            <div>
+              <FormLabel>Categoria</FormLabel>
+              <Select
+                name="category"
+                placeholder="Selecione uma categoria"
+                value={product?.category}
+                w={300}
+                onChange={handleChange}
+              >
+                {categories?.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+              </Select>
+            </div>
+
+            <div>
+              <FormLabel>Avaliação</FormLabel>
+              <NumericFormat
+                onChange={handleChange}
+                name="rating"
+                value={product?.rating}
+                customInput={Input}
+                w={300}
+              />
+            </div>
+
+            <div>
+              <FormLabel>Estoque</FormLabel>
+              <NumericFormat
+                onChange={handleChange}
+                name="stock"
+                value={product?.stock}
+                customInput={Input}
+                w={300}
+              />
+            </div>
+          </div>
+
+          <div className={style.container} style={{ marginTop: "30px" }}>
+            <Card maxW="auto">
+              <CardHeader>
+                <Text as="b">Imagem principal</Text>
+              </CardHeader>
+              <CardBody
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+              >
                 <Image
-                  key={image}
-                  src={image}
+                  src={product?.thumbnail ?? emptyImage}
                   alt={product?.title}
                   width={300}
                   height="auto"
                 />
-              ))}
-            </CardBody>
-          </Card>
-        </div>
-      </FormControl>
+                <input
+                  type="file"
+                  accept="image/*"
+                  name="thumbnail"
+                  id="thumbnail"
+                  onChange={handleFileChange}
+                  className={style["input-file"]}
+                />
+                <label htmlFor="thumbnail" className={style["label-file"]}>
+                  Troque a imagem
+                </label>
+              </CardBody>
+            </Card>
+
+            <Card maxW="auto">
+              <CardHeader>
+                <Text as="b">Imagens</Text>
+              </CardHeader>
+              <CardBody
+                display="flex"
+                justifyContent="center"
+                flexWrap="wrap"
+                gap="20px"
+              >
+                {product?.images.map((image) => (
+                  <Image
+                    key={image}
+                    src={image}
+                    alt={product?.title}
+                    width={300}
+                    height="auto"
+                  />
+                ))}
+              </CardBody>
+            </Card>
+          </div>
+        </FormControl>
+      </Box>
     </>
   );
 }
--- src/pages/Products/EditProduct/service.ts
+++ src/pages/Products/EditProduct/service.ts
@@ -1,13 +1,27 @@
 import axios from "axios";
-import { ProductsType } from "../../../types/Products";
+import { ProductType } from "../context/ProductContext";
 
 export function GetProductById(id: number) {
-  return new Promise<ProductsType>((resolve) => {
+  return new Promise<ProductType>((resolve) => {
     axios
       .get(`https://dummyjson.com/products/${id}`)
       .then((res) => {
-        const product: ProductsType = res.data;
+        const product: ProductType = res.data;
         resolve(product);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+}
+
+export function GetCategories() {
+  return new Promise<string[]>((resolve) => {
+    axios
+      .get(`https://dummyjson.com/products/categories`)
+      .then((res) => {
+        const categories: string[] = res.data;
+        resolve(categories);
       })
       .catch((err) => {
         console.error(err);
