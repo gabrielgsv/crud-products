1064ae6aecbda99cf4af72e3adac60242ffa1f45
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-10-21 17:13:21 -0300

    fixed grid layout with text limits

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {
+  Button,
+  Card,
+  Input,
   Table,
   TableContainer,
   Tbody,
@@ -9,43 +13,96 @@ import {
 } from "@chakra-ui/react";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { useEffect, useState } from "react";
+import { FiPlus } from "react-icons/fi";
+import { Link } from "react-router-dom";
 import packageAnimation from "../../assets/package-animation.json";
+import { useDebounce } from "../../hooks/UseDebounce";
 import ActionButtons from "./components/ActionButtons";
-import { ProductType } from "./context/ProductContext";
-import { getProducts } from "./service";
+import {
+  DEFAULT_VALUE,
+  ProductType,
+  useProduct,
+} from "./context/ProductContext";
+import { getAndSearchProducts } from "./service";
 import style from "./style.module.css";
 export default function Products() {
   const [products, setProducts] = useState<ProductType[]>([]);
+  const [searchText, setSearchText] = useState<string>("");
+  const debouncedSearchText = useDebounce<string>(searchText, 500);
+
+  const { setProduct } = useProduct();
 
   useEffect(() => {
-    getProducts.then((res) => {
+    setProduct(DEFAULT_VALUE.product);
+  }, []);
+
+  useEffect(() => {
+    getAndSearchProducts(searchText).then((res) => {
       setProducts(res);
     });
-  }, []);
+  }, [debouncedSearchText]);
+
+  function handleChangeSearch(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchText(e.target.value);
+  }
 
   return (
     <>
       <div className={style.container}>
-        <Player autoplay src={packageAnimation} keepLastFrame />
-        <TableContainer>
-          <Table variant="simple" className={style.table}>
-            <Thead>
-              <Tr>
-                <Th>Nome</Th>
-                <Th>Ações</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {products.map((product) => (
-                <Tr key={product.id}>
-                  <Td>{product.title}</Td>
-                  <Td>
-                    <ActionButtons id={product.id as number} setProducts={setProducts} />
-                  </Td>
+        <Player autoplay src={packageAnimation} keepLastFrame speed={0.5} />
+        <TableContainer mt={10}>
+          <Card
+            variant="outline"
+            rounded="lg"
+            maxWidth="90vw"
+            minWidth={"500px"}
+            p={10}
+            display="flex"
+            flexDir="column"
+            alignItems="center"
+          >
+            <Input
+              placeholder="Pesquisar"
+              width={300}
+              mb={5}
+              onChange={handleChangeSearch}
+            />
+            <Link to="/create-product" style={{ alignSelf: "flex-end" }}>
+              <Button
+                colorScheme="blue"
+                variant="outline"
+                leftIcon={<FiPlus />}
+              >
+                Criar produto
+              </Button>
+            </Link>
+            <br />
+            <Table size="sm">
+              <Thead>
+                <Tr>
+                  <Th>Nome</Th>
+                  <Th>Ações</Th>
                 </Tr>
-              ))}
-            </Tbody>
-          </Table>
+              </Thead>
+              <Tbody>
+                {products.map((product) => (
+                  <Tr key={product.id}>
+                    <Td>
+                      <div className={style["product-text"]}>
+                        {product.title}
+                      </div>
+                    </Td>
+                    <Td>
+                      <ActionButtons
+                        id={product.id as number}
+                        setProducts={setProducts}
+                      />
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </Card>
         </TableContainer>
       </div>
     </>
--- src/pages/Products/service.ts
+++ src/pages/Products/service.ts
@@ -1,17 +1,20 @@
 import api from "../../services/api";
 import { ProductType } from "./context/ProductContext";
 
-export const getProducts = new Promise<ProductType[]>((resolve) => {
-  api
-    .get("/products")
-    .then((res) => {
-      const products: ProductType[] = res.data.products;
-      resolve(products);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-});
+export function getAndSearchProducts(search?: string) {
+  return new Promise<ProductType[]>((resolve) => {
+    const url = search ? `/products/search?q=${search}` : "/products";
+    api
+      .get(url)
+      .then((res) => {
+        const products: ProductType[] = res.data.products;
+        resolve(products);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+}
 
 export function deleteProduct(id: number) {
   return new Promise((resolve) => {
